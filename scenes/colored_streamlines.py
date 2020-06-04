5adac8177cf6541b1ff0f1d84b72f8ccb19cd64a
FedeClaudi <federicoclaudi@protonmail.com>
2020-06-04 20:21:37 +0100

    bump

--- scenes/colored_streamlines.py
+++ scenes/colored_streamlines.py
@@ -1,11 +1,20 @@
+"""
+    This tutorial shows you how to render efferent mesoscale connectivity
+    data from the Allen mouse connectome project as streamlines coloring
+    each injection's streamline individually. 
+
+"""
 import brainrender
 from brainrender.scene import Scene
 from brainrender.colors import makePalette
+
 
 # Change some of the default settings
 brainrender.BACKGROUND_COLOR = 'blackboard'
 brainrender.ROOT_COLOR = 'white'
 brainrender.ROOT_ALPHA = .2
+brainrender.SHADER_STYLE = 'cartoon'
+
 
 # Start by creating a scene with the allen brain atlas atlas
 scene = Scene()
@@ -19,6 +28,12 @@ data = data[:2]
 colors = makePalette(len(data), "salmon", "lightgreen")
 actors = scene.add_streamlines(data, color=colors, show_injection_site=False)
 
+# Cut scene in half
+scene.cut_actors_with_plane(
+    "sagittal", showplane=False
+)  # Set showplane to True if you want to see the plane location
+
+
 # Now mody the color of each actor
 # Color each actor's vertices based on the X position
 color_combos = [['darkblue', 'powderblue'], ['deeppink', 'lightpink']]
@@ -26,5 +41,7 @@ for actor, colors in zip(actors, color_combos):
     scals = actor.points()[:, 0]   # pick x coordinates of vertices
     cmap = makePalette(len(scals), colors[0], colors[1]) # make colors
     actor.pointColors(scals, cmap=cmap)
+
+
 
 scene.render(camera="sagittal", zoom=1)
